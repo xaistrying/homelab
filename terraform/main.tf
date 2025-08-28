5d824b7d49f136eaaf81693e5460be89fd51a4f7
xaistrying <xaistrying@gmail.com>
2025-08-29 00:12:13 +0700

    feat: add Terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  cloud { 
+    organization = "xaistrying" 
+    workspaces { 
+      name = "workspace-xaistrying" 
+    } 
+  } 
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "Southeast Asia"
+  tags = {
+    environment = "dev"
+  }
+}
