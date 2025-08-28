2be300e1385f5fe19ff1c0cadc0ec0332da4490c
xaistrying <xaistrying@gmail.com>
2025-08-29 00:22:37 +0700

    chore: minor change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,19 @@
 terraform {
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
   }
+
   cloud { 
     organization = "xaistrying" 
     workspaces { 
       name = "workspace-xaistrying" 
     } 
   } 
+  
 }
 
 provider "azurerm" {
